eda6dc0b103f8d65cf24076b3f9936bc4d8c487a
Hein Thura Wynn @ Thomaz <heinthurawynn.developer@gmail.com>
2025-07-05 11:11:08 +0700

    Rebuild with correct base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Root path for custom domain
+  base: '/',  // Root path for custom domain
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
